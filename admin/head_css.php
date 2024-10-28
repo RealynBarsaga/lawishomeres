e584a644afb66b416cd56c0bd519c283a733ce88
Gilbert Sevillejo <realynb704@gmail.com>
2024-10-28 23:04:03 +0800

    Files upload

--- admin/head_css.php
+++ admin/head_css.php
@@ -11,7 +11,7 @@
     <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
     <link href="../js/morris/morris-0.4.3.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="../css/AdminLTE.css" rel="stylesheet" type="text/css" />
+    <link href="../../css/AdminLTE.css" rel="stylesheet" type="text/css" />
     <link href="../css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
     <link href="../css/select2.css" rel="stylesheet" type="text/css" />
     <script src="../js/jquery-1.12.3.js" type="text/javascript"></script>
