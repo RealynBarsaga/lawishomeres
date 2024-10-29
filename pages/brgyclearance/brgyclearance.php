0b716ba21255b65f755964dbb6eb5a680c3a8b66
Gilbert Sevillejo <realynb704@gmail.com>
2024-10-29 11:33:43 +0800

    Files upload

--- pages/brgyclearance/brgyclearance.php
+++ pages/brgyclearance/brgyclearance.php
@@ -7,7 +7,7 @@
         header('Location: ../../login.php');
         exit; // Ensure no further execution after redirect
     }
-    include('../../head_css.php'); // Removed ob_start() since it's not needed here
+    include('../head_css.php'); // Removed ob_start() since it's not needed here
     ?>
     <style>
         .nav-tabs li a {
