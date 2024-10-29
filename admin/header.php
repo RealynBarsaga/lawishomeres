9ef44520a05985ab47d8f8d8d6ebac3684a4e2be
Gilbert Sevillejo <realynb704@gmail.com>
2024-10-29 14:25:33 +0800

    Files upload

--- admin/header.php
+++ admin/header.php
@@ -2,8 +2,21 @@
 // Start output buffering and session
 ob_start();
 
-// Database connection (assuming $con is the database connection)
-require_once('connection.php');
+// Database credentials
+$MySQL_username = "u510162695_db_barangay";
+$Password = "1Db_barangay";    
+$MySQL_database_name = "u510162695_db_barangay";
+
+// Establishing connection with server
+$con = mysqli_connect('localhost', $MySQL_username, $Password, $MySQL_database_name);
+
+// Checking connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Setting the default timezone
+date_default_timezone_set("Asia/Manila");
 
 // Fetch the total number of notifications
 $countQuery = mysqli_query($con, "SELECT COUNT(*) AS count FROM tbllogs");
