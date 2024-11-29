d7e85f996904f3e7a1b971d654491e9f09d46ee8
Gilbert Sevillejo <realynb704@gmail.com>
2024-11-30 01:17:30 +0800

    Files upload

--- pages/clearance/add_modal.php
+++ pages/clearance/add_modal.php
@@ -52,107 +52,107 @@ $formatted_clearance_number = str_pad($next_clearance_number, 4, '0', STR_PAD_LE
 mysqli_stmt_close($stmt);
 ?>
 <!-- ========================= MODAL ======================= -->
-            <div id="addModal" class="modal fade">
-            <form method="post">
-              <div class="modal-dialog modal-sm" style="width:300px !important;">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title">Manage Clearance</h4>
+<div id="addModal" class="modal fade">
+<form method="post">
+  <div class="modal-dialog modal-sm" style="width:300px !important;">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title">Manage Clearance</h4>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-md-12">
+                <div class="form-group">
+                        <label>Resident Name:</label>
+                        <input name="txt_name" class="form-control input-sm" type="text" placeholder="Name" required/>
                     </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-md-12">
-                            <div class="form-group">
-                                    <label>Resident Name:</label>
-                                    <input name="txt_name" class="form-control input-sm" type="text" placeholder="Name" required/>
-                                </div>
-                                <div class="form-group">
-                                    <label>Clearance #:</label>
-                                    <input name="txt_cnum" class="form-control input-sm" type="text" value="<?php echo $formatted_clearance_number; ?>" readonly/>
-                                </div>
-                                <div class="form-group">
-                                    <label>Purpose:</label>
-                                    <input name="txt_purpose" class="form-control input-sm" type="text" placeholder="Purpose" required/>
-                                </div>
-                                <!-- Age -->
-                                <div class="form-group">
-                                    <label>Age:</label>
-                                    <input name="txt_age" id="txt_age" class="form-control input-sm" type="text" placeholder="Age" readonly/>
-                                </div>
-                                <!-- Birthdate -->
-                                <div class="form-group">
-                                    <label>Birthdate:</label>
-                                    <input name="txt_bdate" id="txt_bdate" class="form-control input-sm" type="date" placeholder="Birthdate" max="<?php echo date('Y-m-d'); ?>" required/>
-                                </div>
-                                <div class="form-group">
-                                    <?php
-                                        $off_barangay = $_SESSION['barangay'];
-                                
-                                        // Purok options for each barangay
-                                        $puroks = [
-                                            "Tabagak" => ["Lamon-Lamon", "Tangigue", "Lawihan", "Lower-Bangus", "Upper-Bangus"],
-                                            "Bunakan" => ["Bilabid", "Helinggero", "Kamaisan", "Kalubian", "Samonite"],
-                                            /* "Kodia" => ["Purok X", "Purok Y", "Purok Z"], */
-                                            /* "Talangnan" => ["",], */
-                                            /*  "Poblacion" => ["",], */
-                                            "Maalat" => ["Neem Tree", "Talisay", "Kabakhawan", "Mahogany", "Gmelina"],
-                                            "Pili" => ["Malinawon", "Mahigugmaon", "Matinabangun", "Maabtikon", "Malipayon", "Mauswagon"],
-                                            /* "Kaongkod" => ["Purok", "Puroks"], */
-                                            /* "Mancilang" => ["Purok", "Puroks"], */
-                                            /* "Kangwayan" => ["Purok", "Puroks"], */
-                                            /* "Kangwayan" => ["Purok", "Puroks"], */
-                                            /* "Tugas" => ["Purok", "Puroks"], */
-                                            /* "Malbago" => ["Purok", "Puroks"], */
-                                            "Tarong" => ["Orchids", "Gumamela", "Santan", "Rose", "Vietnam Rose", "Kumintang", "Sunflower", "Daisy"],
-                                            // Add purok options for other barangays
-                                        ];
-                                    ?>
-                                    <label>Purok:</label>
-                                    <select name="txt_purok" class="form-control input-sm" required>
-                                        <option value="">Select Purok</option>
-                                        <?php
-                                            if (array_key_exists($off_barangay, $puroks)) {
-                                                foreach ($puroks[$off_barangay] as $purok) {
-                                                    echo "<option value=\"$purok\">$purok</option>";
-                                                }
-                                            }
-                                        ?>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label>Birth Place:</label>
-                                    <input name="txt_bplace" class="form-control input-sm" type="text" placeholder="Birth Place" required/>
-                                </div>
-                                <div class="form-group">
-                                   <label class="control-label">Civil Status:</label>
-                                   <select name="txt_cstatus" class="form-control input-sm" required>
-                                        <option value="" disabled selected>Select Civil Status</option>
-                                        <option value="Single">Single</option>
-                                        <option value="Married">Married</option>
-                                        <option value="Widowed">Widowed</option>
-                                   </select>
-                                </div>
-                                <div class="form-group">
-                                    <label>OR Number:</label>
-                                    <input name="txt_ornum" class="form-control input-sm" type="number" placeholder="OR Number" required/>
-                                </div>
-                                <div class="form-group">
-                                    <label>Amount:</label>
-                                    <input name="txt_amount" class="form-control input-sm" type="number" placeholder="Amount" required/>
-                                </div>
-                            </div>
-                        </div>
-                        
+                    <div class="form-group">
+                        <label>Clearance #:</label>
+                        <input name="txt_cnum" class="form-control input-sm" type="text" value="<?php echo $formatted_clearance_number; ?>" readonly/>
                     </div>
-                    <div class="modal-footer">
-                        <input type="button" class="btn btn-default btn-sm" data-dismiss="modal" value="Cancel"/>
-                        <input type="submit" class="btn btn-primary btn-sm" name="btn_add" value="Add"/>
+                    <div class="form-group">
+                        <label>Purpose:</label>
+                        <input name="txt_purpose" class="form-control input-sm" type="text" placeholder="Purpose" required/>
+                    </div>
+                    <!-- Age -->
+                    <div class="form-group">
+                        <label>Age:</label>
+                        <input name="txt_age" id="txt_age" class="form-control input-sm" type="text" placeholder="Age" readonly/>
+                    </div>
+                    <!-- Birthdate -->
+                    <div class="form-group">
+                        <label>Birthdate:</label>
+                        <input name="txt_bdate" id="txt_bdate" class="form-control input-sm" type="date" placeholder="Birthdate" max="<?php echo date('Y-m-d'); ?>" required/>
+                    </div>
+                    <div class="form-group">
+                        <?php
+                            $off_barangay = $_SESSION['barangay'];
+                    
+                            // Purok options for each barangay
+                            $puroks = [
+                                "Tabagak" => ["Lamon-Lamon", "Tangigue", "Lawihan", "Lower-Bangus", "Upper-Bangus"],
+                                "Bunakan" => ["Bilabid", "Helinggero", "Kamaisan", "Kalubian", "Samonite"],
+                                /* "Kodia" => ["Purok X", "Purok Y", "Purok Z"], */
+                                /* "Talangnan" => ["",], */
+                                /*  "Poblacion" => ["",], */
+                                "Maalat" => ["Neem Tree", "Talisay", "Kabakhawan", "Mahogany", "Gmelina"],
+                                "Pili" => ["Malinawon", "Mahigugmaon", "Matinabangun", "Maabtikon", "Malipayon", "Mauswagon"],
+                                /* "Kaongkod" => ["Purok", "Puroks"], */
+                                /* "Mancilang" => ["Purok", "Puroks"], */
+                                /* "Kangwayan" => ["Purok", "Puroks"], */
+                                /* "Kangwayan" => ["Purok", "Puroks"], */
+                                /* "Tugas" => ["Purok", "Puroks"], */
+                                /* "Malbago" => ["Purok", "Puroks"], */
+                                "Tarong" => ["Orchids", "Gumamela", "Santan", "Rose", "Vietnam Rose", "Kumintang", "Sunflower", "Daisy"],
+                                // Add purok options for other barangays
+                            ];
+                        ?>
+                        <label>Purok:</label>
+                        <select name="txt_purok" class="form-control input-sm" required>
+                            <option value="">Select Purok</option>
+                            <?php
+                                if (array_key_exists($off_barangay, $puroks)) {
+                                    foreach ($puroks[$off_barangay] as $purok) {
+                                        echo "<option value=\"$purok\">$purok</option>";
+                                    }
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label>Birth Place:</label>
+                        <input name="txt_bplace" class="form-control input-sm" type="text" placeholder="Birth Place" required/>
+                    </div>
+                    <div class="form-group">
+                       <label class="control-label">Civil Status:</label>
+                       <select name="txt_cstatus" class="form-control input-sm" required>
+                            <option value="" disabled selected>Select Civil Status</option>
+                            <option value="Single">Single</option>
+                            <option value="Married">Married</option>
+                            <option value="Widowed">Widowed</option>
+                       </select>
+                    </div>
+                    <div class="form-group">
+                        <label>OR Number:</label>
+                        <input name="txt_ornum" class="form-control input-sm" type="number" placeholder="OR Number" required/>
+                    </div>
+                    <div class="form-group">
+                        <label>Amount:</label>
+                        <input name="txt_amount" class="form-control input-sm" type="number" placeholder="Amount" required/>
                     </div>
                 </div>
-              </div>
-              </form>
             </div>
+            
+        </div>
+        <div class="modal-footer">
+            <input type="button" class="btn btn-default btn-sm" data-dismiss="modal" value="Cancel"/>
+            <input type="submit" class="btn btn-primary btn-sm" name="btn_add" value="Add"/>
+        </div>
+    </div>
+  </div>
+  </form>
+</div>
 <script>
     $(document).ready(function() {
         // Calculate age function
