4f799af8c655827770e53a5e3fb616bc889081a5
Gilbert Sevillejo <realynb704@gmail.com>
2024-11-29 17:37:27 +0800

    Files upload

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php
 // Set cookie parameters before starting the session
 session_set_cookie_params([
@@ -122,8 +124,6 @@ if ($error || $error_attempts) {
     $error_message = ""; // Reset error message if login attempt is successful
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
