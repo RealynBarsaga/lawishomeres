f59d3a2202f30b39ba1e48d86538aab8f461538d
Gilbert Sevillejo <realynb704@gmail.com>
2024-11-29 22:12:32 +0800

    Files upload

--- admin/permit/permit_form.php
+++ admin/permit/permit_form.php
@@ -75,7 +75,7 @@
     <!-- Header Section -->
     <div class="header-section" style="margin-bottom: 30px;">
         <!-- Left Logo -->
-        <div class="col-xs-4 col-sm-3 col-md-2" style="padding: 10px; float: left;margin-top: 40px;">
+        <div class="col-xs-4 col-sm-3 col-md-2" style="padding: 10px; float: left;margin-top: 45px;">
             <img src="../../img/lg.png" style="width: 170px; height: 135px;" />
         </div>
         <!-- Centered Text Content -->
