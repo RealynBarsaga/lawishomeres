123516f165fafbf62aa5bf8c6656fe1ad3cc7245
Gilbert Sevillejo <realynb704@gmail.com>
2024-11-29 14:16:19 +0800

    Files upload

--- email_link_form.php
+++ email_link_form.php
@@ -288,7 +288,7 @@ $email = '';
     <div class="container">
         <div class="row">
             <div class="col-md-4 offset-md-4 form">
-                <form action="../email_link_process" method="POST" autocomplete="off">
+                <form action="" method="POST" autocomplete="off">
                     <p class="res">Reset via Email Link</p>
                     <p class="text-center">Enter the email address associated with your account and we will send you a link to reset your password.</p>
                     <br>
