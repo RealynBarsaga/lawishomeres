8192f9fb97dc29ef63219cf4841498aa37528617
Gilbert Sevillejo <realynb704@gmail.com>
2024-11-29 21:22:20 +0800

    Files upload

--- admin/permit/permit_form.php
+++ admin/permit/permit_form.php
@@ -69,7 +69,7 @@
 
     ob_start(); // Start output buffering
     include('../head_css.php'); // Include CSS file
-    include "../../connection.php"; // Include database connection
+    include "../connection.php"; // Include database connection
     ?>
     
     <!-- Header Section -->
@@ -82,7 +82,7 @@
         <!-- Centered Text Content -->
         <div class="col-xs-7 col-sm-6 col-md-8" style="text-align: center;">
             <div style="font-size: 16px; font-family: 'Courier New', Courier;">
-                <p>Republic of the Philippines<br>
+                <p style="margin-right: 170px;">Republic of the Philippines<br>
                 Province of Cebu<br>
                 <span style="font-size: 18px; font-weight: bolder;margin-left: -18px;">Municipality of Madridejos</span></p>
             </div>
