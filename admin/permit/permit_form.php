53239252b555894c350b564fd91cd3ece041bb95
Gilbert Sevillejo <realynb704@gmail.com>
2024-11-29 21:33:58 +0800

    Files upload

--- admin/permit/permit_form.php
+++ admin/permit/permit_form.php
@@ -81,7 +81,7 @@
         
         <!-- Centered Text Content -->
         <div class="col-xs-7 col-sm-6 col-md-8" style="text-align: center;">
-            <div style="font-size: 16px; font-family: 'Courier New', Courier;">
+            <div style="font-size: 15px; font-family: 'Courier New', Courier;">
                 <p style="margin-right: 180px;">Republic of the Philippines<br>
                 Province of Cebu<br>
                 <span style="font-size: 18px; font-weight: bolder;margin-left: -1px;">Municipality of Madridejos</span></p>
