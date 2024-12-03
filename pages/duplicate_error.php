88985f2d679da5094e19f30f8babd05172986c6e
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-04 04:22:00 +0800

    Files upload

--- pages/duplicate_error.php
+++ pages/duplicate_error.php
@@ -55,7 +55,7 @@ echo '<div class="alert alert-filesize alert-autocloseable-filesize" style="back
     echo '<script>$(document).ready(function (){file_upload_error();});</script>';
     unset($_SESSION['file_upload_error']);
     } 
-echo '<div class="alert alert-filesize alert-autocloseable-filesize" style="background: #d9534f; position: fixed; top: 1em; right: 1em; z-index: 9999; display: none;">
+echo '<div class="alert alert-file_upload_error alert-autocloseable-file_upload_error" style="background: #d9534f; position: fixed; top: 1em; right: 1em; z-index: 9999; display: none;">
      Sorry, there was an error uploading your file !
 </div>';
 ?>
@@ -64,7 +64,7 @@ echo '<div class="alert alert-filesize alert-autocloseable-filesize" style="back
     echo '<script>$(document).ready(function (){invalid_image();});</script>';
     unset($_SESSION['invalid_image']);
     } 
-echo '<div class="alert alert-filesize alert-autocloseable-filesize" style="background: #d9534f; position: fixed; top: 1em; right: 1em; z-index: 9999; display: none;">
+echo '<div class="alert alert-invalid_image alert-autocloseable-invalid_image" style="background: #d9534f; position: fixed; top: 1em; right: 1em; z-index: 9999; display: none;">
      File is not a valid image !
 </div>';
 ?>
@@ -73,7 +73,7 @@ echo '<div class="alert alert-filesize alert-autocloseable-filesize" style="back
     echo '<script>$(document).ready(function (){invalid_file();});</script>';
     unset($_SESSION['invalid_file']);
     } 
-echo '<div class="alert alert-filesize alert-autocloseable-filesize" style="background: #d9534f; position: fixed; top: 1em; right: 1em; z-index: 9999; display: none;">
+echo '<div class="alert alert-invalid_file alert-autocloseable-invalid_file" style="background: #d9534f; position: fixed; top: 1em; right: 1em; z-index: 9999; display: none;">
      Invalid file type or size !
 </div>';
 ?>
