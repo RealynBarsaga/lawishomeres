1d9b3943ec765d1f59b463ccb3e0aae274e73f44
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-04 16:01:29 +0800

    Files upload

--- login.php
+++ login.php
@@ -144,7 +144,7 @@ if ($error || $error_attempts) {
     <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     <!-- Theme style -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="https://dashboard.hcaptcha.com/overview" async defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
