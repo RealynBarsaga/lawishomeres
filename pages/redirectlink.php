e744cb15aff3ca373635ab66cefd9a7881c819e3
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-07 02:51:30 +0800

    Files upload

--- pages/redirectlink.php
+++ pages/redirectlink.php
@@ -2,13 +2,54 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>403 Forbidden</title>
+    <title>404: This page could not be found</title>
+    <style>
+        body {
+            font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+            color: #000;
+            background: #fff;
+            margin: 0;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+        }
+        @media (prefers-color-scheme: dark) {
+            body {
+                color: #fff;
+                background: #000;
+            }
+        }
+        h4 {
+            font-size: 2rem;
+            margin: 0;
+            padding-right: 1rem;
+            border-right: 1px solid rgba(0, 0, 0, 0.3);
+        }
+        @media (prefers-color-scheme: dark) {
+            h1 {
+                border-right: 1px solid rgba(255, 255, 255, 0.3);
+            }
+        }
+        h2 {
+            font-size: 1rem;
+            font-weight: 400;
+            margin: 0;
+            line-height: 1.5;
+        }
+        .container {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>403 Forbidden</h1>
-    <p>You do not have permission to access this page.</p>
-    <a href="/">Return to homepage</a>
+    <div class="container">
+        <h4>404</h4>
+        <h2>&nbsp;&nbsp;This page could not be found.</h2>
+    </div>
 </body>
 </html>
