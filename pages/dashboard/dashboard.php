a21169d9c828c2a4bbf9eb270ae6b02fdb9218ee
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-07 02:26:23 +0800

    Files upload

--- pages/dashboard/dashboard.php
+++ pages/dashboard/dashboard.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['userid'])) {
 // Check if the user's role is 'admin'
 if (!isset($_SESSION['role']) || $_SESSION['role'] === 'Staff') {
     // Redirect to a different page (e.g., error page or login page)
-    header('Location: access-denied');
+    header('Location: ../pages/access-denied');
     exit(); // Stop further script execution
 }
 
