e0733834ce3c0a741d5a3d00c8eb7ae3227c8d14
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-06 23:42:43 +0800

    Files upload

---
+++ cookiesModal.php
@@ -0,0 +1,10 @@
+<div class="wrapper">
+    <div class="cookie-message">
+        <h3>We use cookies!</h3>
+        <p>This website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent.</p>
+    </div>
+    <div class="buttons">
+        <button id="acceptBtn">Accept</button>
+        <button id="rejectBtn">Reject</button>
+    </div>
+</div>
--- login.php
+++ login.php
@@ -895,16 +895,7 @@ if(isset($_POST['submit']))
 <!-- Terms and Conditions Modal -->
 <?php include 'termsModal.php'; ?>
 <!-- Cookie Consent Banner -->
-<div class="wrapper">
-    <div class="cookie-message">
-        <h3>We use cookies!</h3>
-        <p>This website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent.</p>
-    </div>
-    <div class="buttons">
-        <button id="acceptBtn">Accept</button>
-        <button id="rejectBtn">Reject</button>
-    </div>
-</div>
+<?php include 'cookiesModal.php'; ?>
 <script>
      // Handle the OK button for modal
      document.addEventListener("DOMContentLoaded", function() {
