1a21d801381c10e276be93b87e51b8bba9620be3
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-07 00:46:20 +0800

    Files upload

--- logout.php
+++ logout.php
@@ -1,23 +1,11 @@
 <?php
-// Logout script
 session_start();
 
-// Unset all session variables
-$_SESSION = [];
-
-// Destroy the session
+// Destroy all sessions
+session_unset();
 session_destroy();
 
-// Delete the session cookie
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000, 
-        $params["path"], $params["domain"], 
-        $params["secure"], $params["httponly"]
-    );
-}
-
-// Redirect to login or home page
+// Redirect to login page
 header("Location: login.php");
 exit();
 ?>
