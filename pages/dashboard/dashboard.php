db0981e763dbfeee4eca8020209fa29e284515c4
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-07 22:14:31 +0800

    Files upload

--- pages/dashboard/dashboard.php
+++ pages/dashboard/dashboard.php
@@ -12,7 +12,7 @@ session_set_cookie_params([
 session_start();
 
 // Custom session timeout (e.g., 30 minutes)
-$timeout = 1800; // 30 minutes
+$timeout = 60; // 30 minutes
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $timeout)) {
     session_unset();
     session_destroy();
