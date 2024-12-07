44f594fef56b809a08cd233bc31d7f4558fa4dd6
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-07 10:54:49 +0800

    Files upload

--- forgot_password_option.php
+++ forgot_password_option.php
@@ -1,14 +1,3 @@
-<?php
-// Start the session
-session_start();
-
-// Check if the user's role is not 'Staff'
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'Staff') {
-    // Redirect to the access denied page if not 'Staff'
-    header('Location: redirectlink');
-    exit(); // Stop further script execution
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
