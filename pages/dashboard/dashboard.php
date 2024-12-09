7ee429164154ad4c9616a0191252a2a2cbd4df78
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-09 22:10:34 +0800

    Files upload

--- pages/dashboard/dashboard.php
+++ pages/dashboard/dashboard.php
@@ -14,6 +14,17 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'Staff') {
     exit(); // Stop further script execution
 }
 
+// Session timeout logic (15 minutes)
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > 5)) {
+    session_unset();
+    session_destroy();
+    header('Location: ../../login.php');
+    exit();
+}
+
+$_SESSION['last_activity'] = time(); // Update last activity timestamp
+
+
 // If the user is logged in and their role is correct, include the necessary files
 include('../head_css.php');
 ?>
