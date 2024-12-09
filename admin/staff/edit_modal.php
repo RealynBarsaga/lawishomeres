c69c4ba635dc5e93a6bb570cfb365693ce641407
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-10 00:49:42 +0800

    Files upload

--- admin/staff/edit_modal.php
+++ admin/staff/edit_modal.php
@@ -35,7 +35,7 @@
                 <div class="form-group">
                     <label>Password: </label>
                     <div class="input-group">
-                        <input id="password" name="txt_edit_pass" class="form-control input-sm" type="password" placeholder="•••••••••••"/>
+                        <input id="password" name="txt_edit_pass" class="form-control input-sm" type="password" value="'.$row['password'].'"/>
                         <span class="input-group-addon eye-icon" onclick="togglePasswordVisibility(\'password\')">
                             <i class="fa fa-eye" id="eye-password"></i>
                         </span>
