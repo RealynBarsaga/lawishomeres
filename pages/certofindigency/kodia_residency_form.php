39acd4f17ddb6caa46fa5079e6e2ff31d33f4da4
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-11 21:24:14 +0800

    Files upload

--- pages/certofindigency/kodia_residency_form.php
+++ pages/certofindigency/kodia_residency_form.php
@@ -170,8 +170,8 @@
                 while($row = mysqli_fetch_array($qry)){
                     if($row['sPosition'] == "Secretary"){
                         echo '
-                        <strong style="font-size: 18px; margin-left: -250px;">HON.'.strtoupper($row['completeName']).'</strong><br>
-                        <hr style="border: 1px solid black; width: 50%; margin: 1px auto;margin-top: -15px;" />
+                        <strong style="font-size: 18px; margin-left: -300px;">HON.'.strtoupper($row['completeName']).'</strong><br>
+                        <hr style="border: 1px solid black; width: 50%; margin-left: -300px;margin-top: -15px;" />
                         <span style="margin-left: 85px;">Barangay Secretary</span>
                         ';
                     }
