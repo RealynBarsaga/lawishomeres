a6e8a60ad1fd632f51ab439ccb8408c7f71672e4
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-11 21:21:50 +0800

    Files upload

--- pages/certofindigency/kodia_residency_form.php
+++ pages/certofindigency/kodia_residency_form.php
@@ -154,9 +154,7 @@
                     </span>";
                 }
             ?>
-        </p>
-    </div>
-    <br>
+         <br>
     <br>
     <br>
     <br>
@@ -180,6 +178,8 @@
                 }
             ?>
         </p>
+        </p>
+    </div>
     <div class="col-xs-offset-6 col-xs-5 col-md-offset-6 col-md-4" style="top: 280px;">
     <i>Approved by:</i>
     <br>
