87b402668ff98414137e71180c46b4e9ebd2eef0
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-11 08:12:36 +0800

    Files upload

--- pages/clearance/tarong_clearance_form.php
+++ pages/clearance/tarong_clearance_form.php
@@ -172,10 +172,9 @@
             // Loop through clearance details
             if ($row = mysqli_fetch_array($squery)) {
                 echo "<p style='font-family: \"Courier New\", Courier, monospace; text-align: justify; font-size: 15px;margin-left: 220px;margin-right: 60px;'>
-                &nbsp;&nbsp;&nbsp;This is to certify that <strong>" . strtoupper($row['Name']) . "</strong>, 
-                <strong>" . $row['age'] . "</strong> years old, " . $row['civilstatus'] . ", and a Filipino citizen, is a bona fide resident of Purok " . $row['purok'] . ", Barangay " . $row['barangay'] . ", 
-                Madridejos, Cebu is known to me personally to be a person of good moral character, Peaceful and a law-abiding citizen in this community.<br><br><br>
-                This is to certify further that he/she has no pending case before the office of the Lupong Tagapamayapa either civil or criminal case as of the date</p>";
+                &nbsp;&nbsp;&nbsp;This is to certify that <strong>" . strtoupper($row['Name']) . "</strong> a Filipino citizen of legal Age " . $row['civilstatus'] . " with a residence and address in Purok " . $row['purok'] . ", Barangay " . $row['barangay'] . ", 
+                Madridejos, Cebu is known to me personally to be a person of good moral character, Peaceful and a law-abiding citizen in this community.<br><br>
+                I further certify that he/she has no crimininal record filed with this Barangay nor has he/she ever been convicted or indicted if any offense and that he/she has no derogatory information or whatsoever legal papers maybe presented.</p>";
             }
         ?>
         </p>
