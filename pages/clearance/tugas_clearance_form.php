36d0566844ee4f874561a6aa28ba8c90168e4c69
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-12 00:24:11 +0800

    Files upload

--- pages/clearance/tugas_clearance_form.php
+++ pages/clearance/tugas_clearance_form.php
@@ -233,8 +233,8 @@
           if ($row = mysqli_fetch_array($squery)) {
             echo '
               <strong style="margin-top: -350px;font-size: 17px; margin-left: 250px;">'.strtoupper($row['Name']).'</strong><br>
-              <hr style="border: 0.1px solid black; width: 27%; margin-left: 361px;margin-top: -15px;" />
-              <p style="margin-left: 390px;margin-top: -20px;">Signature of Applicant</p>
+              <hr style="border: 0.1px solid black; width: 27%; margin-left: 261px;margin-top: -15px;" />
+              <p style="margin-left: 250px;margin-top: -20px;">Signature of Applicant</p>
             ';
           }
         ?>
