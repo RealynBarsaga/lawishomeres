29c50683ed7d6fd72bd27784d1c88044927b176e
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-11 21:43:50 +0800

    Files upload

--- pages/certofindigency/kodia_residency_form.php
+++ pages/certofindigency/kodia_residency_form.php
@@ -179,7 +179,7 @@
         </p>
         </p>
     </div>
-    <div class="col-xs-offset-6 col-xs-5 col-md-offset-6 col-md-4" style="top: 280px;">
+    <div class="col-xs-offset-6 col-xs-5 col-md-offset-6 col-md-4" style="top: 250px;">
     <i>Approved by:</i>
     <br>
     <br>
