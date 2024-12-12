8bf12630f229a4aad3eddea4f65ff0c06f1dcc8f
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-12 11:32:36 +0800

    Files upload

--- pages/household/add_modal.php
+++ pages/household/add_modal.php
@@ -25,7 +25,7 @@
                                 <div id="family_members_list" class="form-control input-sm" style="border: 1px solid #ccc; padding: 5px; height: 100px; overflow-y: auto;" readonly>
                                     <!-- Family member names will be dynamically added here -->
                                 </div>
-                                <input id="txt_members" name="txt_members" class="form-control input-sm" type="hidden"/>
+                                <input id="txt_members" name="txt_members" class="form-control input-sm hah" type="hidden"/>
                             </div>
                             <div class="form-group">
                                 <label>Total Household Members:</label>
@@ -94,7 +94,7 @@
 
                     if (familyMembers.length > 0 && familyMembers[0] !== "No family members found") {
                         familyMembers.forEach(function(memberName) {
-                            $('#family_members_list').append('<input type="text" class="form-control input-sm hah" value="' + memberName + '" readonly />');
+                            $('#family_members_list').append('<input type="text" class="form-control input-sm" value="' + memberName + '" readonly />');
                         });
                         $('#txt_members').val(familyMembers.join(', '));
                     } else {
