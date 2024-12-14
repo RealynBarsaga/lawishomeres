996fa7ec206f3e9d6ad97fe4eb4a6226cbbb996f
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-15 01:18:17 +0800

    Files upload

--- admin/login.php
+++ admin/login.php
@@ -212,7 +212,7 @@ if ($error || $error_attempts) {
             color: red; /* Error message color */
             font-size: 12px;
             text-align: left; /* Center error messages */
-            margin-top: 15px;
+            margin-top: 25px;
         }
         /* Responsive adjustments */
         @media (max-width: 768px) {
