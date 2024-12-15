fc28118da3ccaa0b9e7fff1f0bf1c2d6f3feca7f
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-15 16:10:18 +0800

    Files upload

--- admin/householdlist/function.php
+++ admin/householdlist/function.php
@@ -3,7 +3,7 @@ if (isset($_POST['btn_del'])) {
     if (isset($_POST['hidden_id'])) {
         $txt_id = $_POST['hidden_id'];
 
-        $delete_query = mysqli_query($con, "DELETE FROM tblpermit WHERE id = '$txt_id'");
+        $delete_query = mysqli_query($con, "DELETE FROM tblhousehold WHERE id = '$txt_id'");
 
         if ($delete_query) {
             $_SESSION['delete'] = 1;
