d8e28d99b53620b171cd407cfe0b9aab050ee26e
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-17 00:45:35 +0800

    Files upload

--- pages/resident/edit_modal.php
+++ pages/resident/edit_modal.php
@@ -34,8 +34,10 @@ echo '<div id="editModal'.$row['id'].'" class="modal fade" role="dialog">
                             </div> 
                             <div class="col-sm-4">
                                 <input name="txt_edit_mname" class="form-control input-sm" type="text" pattern="^(?!\s*$)[A-Za-z\s.,]+$" value="'.$erow['mname'].'" style="margin-left: -62px;width: 93px;"/>
-
-                                <input name="txt_edit_exname" class="form-control input-sm" type="text" pattern="^(?!\s*$)[A-Za-z\s.,]+$" value="'.$erow['exname'].'" style="width: 76px;margin-left: 313px;margin-top: -29px;"/>
+                               
+                                <div class="col-sm-4">
+                                    <input name="txt_edit_exname" class="form-control input-sm" type="text" pattern="^(?!\s*$)[A-Za-z\s.,]+$" value="'.$erow['exname'].'" style="width: 76px;margin-left: 38px;margin-top: -29px;"/>
+                                </div>
                             </div>
                         </div>
 
