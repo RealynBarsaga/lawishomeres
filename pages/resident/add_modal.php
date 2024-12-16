a32d8ae66a60102b2bbba8cc999a581a3f99cf59
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-17 00:31:26 +0800

    Files upload

--- pages/resident/add_modal.php
+++ pages/resident/add_modal.php
@@ -14,7 +14,7 @@
 
                                 <!-- Name -->
                                 <div class="form-group">
-                                    <label class="control-label">Name: <span style="color:gray; font-size: 10px;">(Lastname Firstname, Middlename ,Extensionname(Jr.,Sr.,I.,II) )</span></label><br>
+                                    <label class="control-label">Name: <span style="color:gray; font-size: 10px;">(Lastname Firstname, Middlename ,Extensionname (Jr.,Sr.,I.,II) )</span></label><br>
                                     <div class="col-sm-4">
                                         <input name="txt_lname" class="form-control input-sm" type="text" placeholder="Lastname" required
                                         pattern="^(?!\s)(?!.*<script>)(?!.*<\/script>).*[\w\s]*$" 
@@ -30,7 +30,7 @@
                                         pattern="^(?!\s)(?!.*<script>)(?!.*<\/script>).*[\w\s]*$" 
                                         title="Spaces and <script></script> tags are not allowed." style="margin-left: -62px;width: 93px;"/>
 
-                                        <input name="txt_exname" class="form-control input-sm" type="text" placeholder="Extensionname"
+                                        <input name="txt_exname" class="form-control input-sm" type="text" placeholder="Extension"
                                         pattern="^(?!\s)(?!.*<script>)(?!.*<\/script>).*[\w\s]*$" 
                                         title="Spaces and <script></script> tags are not allowed." style="width: 76px;margin-left: 38px;margin-top: -29px;"/>
                                     </div>
