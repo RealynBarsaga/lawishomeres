cab513acadb1f668b971a3f22d8bf4313a0aa2b1
Gilbert Sevillejo <realynb704@gmail.com>
2024-12-17 13:37:12 +0800

    Files upload

--- pages/dashboard/dashboard.php
+++ pages/dashboard/dashboard.php
@@ -116,6 +116,7 @@ h3 {
                         ['label' => 'Total Residency', 'icon' => 'fa-file', 'color' => '#f39c12', 'query' => "SELECT * FROM tblrecidency WHERE barangay = '$off_barangay'", 'link' => '../certofresidency/certofres'],
                         ['label' => 'Total Indigency', 'icon' => 'fa-file', 'color' => '#d9534f', 'query' => "SELECT * FROM tblindigency WHERE barangay = '$off_barangay'", 'link' => '../certofindigency/certofindigency'],
                         ['label' => 'Total Brgy Certificate', 'icon' => 'fa-file', 'color' => '#5bc0de', 'query' => "SELECT * FROM tblcertificate WHERE barangay = '$off_barangay'", 'link' => '../brgycertificate/brgycertificate'],
+                        ['label' => 'Total Puroks', 'icon' => 'fa-map-marker', 'color' => '#5bc0de', 'query' => "SELECT COUNT(DISTINCT purok) AS total_puroks FROM tbltabagak WHERE barangay = '$off_barangay'", 'link' => '../resident/resident'],
                     ];
                     
                     foreach ($info_boxes as $box) {
